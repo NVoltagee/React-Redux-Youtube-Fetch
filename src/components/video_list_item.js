92c1d70fddad72c49101b48645d7e67928e60fe6
Norman Trujillo <natrujillo9@gmail.com>
2018-06-04 17:22:33 -0400

    Added new code.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const VideoListItem = ({video}) => {
-    const imgURL = video.snippet.thumbnails.default.url;
-    return (
-        <li className="list-group-item">
-            <div className="video-list media">
-                <div className="media-left">
-                    <img className="media-object" src={imgURL}/>
-                </div>
+const VideoListItem = ({ video, onVideoSelect }) => {
+  const imgURL = video.snippet.thumbnails.default.url;
 
-                <div className="media-body">
-                    <div className="media-heading">{video.snippet.title}</div>
-                </div>
-            </div>
-        </li>
-    );
+  return (
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imgURL} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
